7efeafb694a001349696217e7f962fa746c80193
Aleksandr Malyshev <a.malyshev@ymail.com>
2022-06-07 18:04:39 +0300

    Лабораторная_4

---
+++ lab4.java
@@ -0,0 +1,11 @@
+package com.android;
+
+public class lab4 {
+    public static void main(String[] args) {
+        int j = 7;
+        for (int i = 1; i < 10; i++){
+               System.out.println( i + " * " + j + " = " + i * j);
+           }
+       }
+    }
+
